bd9390f9dd12894e966ae9e52c2652dfad387a6d
KEERAN <rjkeeran@gmail.com>
2021-09-05 11:58:48 +0545

    color flipper

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener("click", function(){
+    const random = getRandomNum();
+    // console.log(random);
+
+    document.body.style.backgroundColor = colors[random];
+    color.textContent = colors[random];
+});
+
+
+function getRandomNum() {
+    return Math.floor(Math.random() * colors.length);
+}
